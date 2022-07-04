3c1809ffaa3fdc621b0c487aed4a85d0bf9e78a1
ALINSHAN <92071951+ERR0R-x-PREDAT0R@users.noreply.github.com>
2022-07-04 10:22:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,9 @@
 //════════════════════════════//
-//If you want to recode, reupload
-//or copy the codes/script,
-//pls give credit
-//no credit? i will take action immediately
+
 //© 2022 Xeon Bot Inc. Cheems Bot MD
-//Thank you to Lord Buddha, Family and Myself
-//════════════════════════════//
-//recode kar ke youtube pe upload kar rhe ya
-//codes copy kar ke apne script me dal rhe
-//hai to, description me xeon ka yt channel
-// ka link paste kr dena as a cradit or github 
-//repo me bhi tag kardena baki jo
-//bhi karna hai apki marzi, thank you!🦄
-//════════════════════════════//
-//If you recode and uploading on your channel
-//or copy pasting the codes in ur script, 
-//i give permission to do as long as you
-//put Xeons youtube channel link in the video
-//description and tag me on githuh repo, 
-//thank you🦄
+
+// Re-edited By @Alinshan
+
 //════════════════════════════//
 
 const fs = require("fs")
@@ -41,26 +26,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['919383491460'] //ur owner number
+global.ownername = "Alinshan"
+//global.ytname = "YT: Xeon" //ur yt chanel name
+//global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.location = "India, Kerala, Kozhikode" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
+global.botname = "Jarvis" //ur bot name
+//global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
+global.packname = "©Jarvis Bot\n\n\n\n\n\n\n"
+global.author = "ᴅᴏɴ'ᴛ sᴛᴇᴀʟ"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
-global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
-global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
-global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
+global.ownertag = ['918592068706'] //ur owner tag
+global.ownernummenu = ['919383491460'] //ur owner number in menu and all
+global.watermark = "©ᴊᴀʀᴠɪs ʙᴏᴛ ɪɴᴄ." //ur watermark
+//global.botscript = "https://github.com/ERR0R-x-PREDAT0R/MD-Bot"
+//global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
+//global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
